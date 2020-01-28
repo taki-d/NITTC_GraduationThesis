1e17aeaaef86a50e05e097a11be5ee2ce6ab8328
taki-d <taka410199@gmail.com>
2020-01-29 04:32:52 +0900

    [fix]fix parameter of release tag

--- .github/actions/latex/entrypoint.sh
+++ .github/actions/latex/entrypoint.sh
@@ -7,9 +7,9 @@ latexmk master.tex
 res=`curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://api.github.com/repos/$GITHUB_REPOSITORY/releases \
 -d "
 {
-  \"tag_name\": \"$(echo ${GITHUB_REF:10})\",
+  \"tag_name\": \"${GITHUB_SHA}\",
   \"target_commitish\": \"$GITHUB_SHA\",
-  \"name\": \"master.pdf $(echo ${GITHUB_REF:10})\",
+  \"name\": \"master.pdf ${GITHUB_SHA}\",
   \"draft\": false,
   \"prerelease\": false
 }"`
